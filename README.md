5e8bc4fe6df4d55df8221474e1e5f386bb75adda
Konstantinos Theofilatos <konstantinos.theofilatos@gmail.com>
2019-07-10 17:20:08 +0200

    adding GP's index.php

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-<h1> Made for plotting efficiency and speed when exploring a ROOT file </h1>
+# qplot stands for quick plot
+It is meant to be used for quickly browsing many ROOT files and produce histograms with ROOT/matplotlib and analyze the data with numpy arrays. 
+
+## Usage
+
+Open many ROOT files, search for known ttrees prodice pointers to them via the ttrees python list:
+
+```
+python -i ~/qplot/qplot.py *.root
+```
+
+or e.g., open one ROOT file and plot different branches, all automatic without worrying for anything but giving the correct variable names:
+
+```
+python -i ~/qplot/qplot.py input.root --var 'jet1_pt,jet2_pt,jet3_pt'
+```
+
+the script will determine range, binning, axis titles, etc for you.
 
--- qplot.py
+++ qplot.py
@@ -20,7 +20,7 @@ def string2list(s):
 import argparse
 parser = argparse.ArgumentParser(description='overlay ROOT histograms from different files')
 parser.add_argument('files', nargs='+', help='needs at minimum 1 file')
-parser.add_argument('-o','--output', default = 'plot_dir', help = 'name of the plot directory to be created')
+parser.add_argument('--output', default = 'qplots', help = 'name of the plot directory to be created')
 parser.add_argument('--ttree', default = '', help = 'name of the tree, if is inside a TDir, Dirname/TreeName, otherwise will attemp to fetch from ListOfKnownTTrees')
 parser.add_argument('--var', nargs='?', help = 'name of the branch, to be printed', default = '')
 parser.add_argument('--goFast','--gf', default = 1.0, type=float, help = 'process a fraction of  the events for each tree')
@@ -34,6 +34,8 @@ parser.add_argument('--logx', help = 'set log scale in the x-axis', action='stor
 parser.add_argument('--nover', help = 'don\'t move overflow to last bin, by default is True',     action='store_false')
 parser.add_argument('--nunder', help = 'don\'t move underflow to first bin, by default is True',  action='store_false')
 parser.add_argument('--debug', help = 'debug mode, verbose print-out',  action='store_true')
+parser.add_argument('--legpos', default = '0.65,0.75,0.93,0.90', help = 'positioning of the legend, default is top-right: 0.65, 0.75 , 0.93, 0.90')
+parser.add_argument('--save', default = '', help = 'give filename of the figures to be saved, will produce filename.pdf and filename.png')
 args = parser.parse_args()
 
 
@@ -73,8 +75,11 @@ no = {'no','n',''}
 ### save a figure
 def save(filename=""):
     os.system("mkdir -p "+args.output)
-    filePDF = args.output+'/'+args.var+'_'+filename+'.pdf' 
-    filePNG = args.output+'/'+args.var+'_'+filename+'.png'
+    if not os.path.exists(args.output+'/index.php'):
+        pathname = os.path.dirname(sys.argv[0])        
+        os.system("cp "+pathname+"/index.php "+args.output)
+    filePDF = args.output+'/'+filename+'.pdf' 
+    filePNG = args.output+'/'+filename+'.png'
     print("saving: \n"+filePDF+"\n"+filePNG)
     can1.SaveAs(filePDF)   
     can1.SaveAs(filePNG)   
@@ -206,6 +211,11 @@ def plotHistos(histos):
     can1.cd()
     if args.logy: can1.SetLogy()
     if args.logx: can1.SetLogx()
+    (lx1, ly1, lx2, ly2) = (float(x) for x in args.legpos.split(','))
+    leg.SetX1NDC(lx1)
+    leg.SetX2NDC(lx2)
+    leg.SetY1NDC(ly1)
+    leg.SetY2NDC(ly2)
     #histos = sorted(histos, key = lambda h : -h.GetBinContent(h.GetMaximumBin()))
     yMin = min(h.GetBinContent(h.GetMinimumBin()) for h in histos)
     yMax = 1.1*max(h.GetBinContent(h.GetMaximumBin()) for h in histos)
@@ -262,7 +272,7 @@ if __name__ == "__main__":
    ### create global pointers to TCanvas and TLegend, histos and define color styles and more
    histos = []
    can1 = ROOT.TCanvas('can1','can1')
-   lx1, ly1, lx2, ly2 = 0.65, 0.8 , 0.93, 0.93
+   (lx1, ly1, lx2, ly2) = (float(x) for x in args.legpos.split(','))
    leg = ROOT.TLegend(lx1, ly1, lx2, ly2)
    colors = [1, 2, 4, 6, 7, 8, 9]
    styles = [1, 2, 3, 4, 5, 6, 7]
@@ -272,169 +282,9 @@ if __name__ == "__main__":
    else: 
        guessMissingArgs(args) 
        makeHistos(histos)
+       if args.save!='':save(args.save)
     
        
-
-
-'''
-### open the ROOT files
-print("opening %s"%args.files)
-tfiles = [ROOT.TFile.Open(f) for f in args.files]
-listOfknownTrees = ['Events','events', 'ntuple/tree']
-ttrees = []
-for tfile in tfiles:
-    if tfile !=None:
-        if args.tree == '':
-            for checkTTreeExistance in listOfknownTrees: 
-                if tfile.Get(checkTTreeExistance) != None:
-                    print("found TTree %s in %s appending it in ttrees"%(checkTTreeExistance, tfile.GetName()))
-                    tfile.ttree = tfile.Get(checkTTreeExistance)
-                    ttrees += [tfile.ttree]            
-        else:
-            print("attempting to fetch %s from %s"%(args.tree, tfile.GetName()))
-            if tfile.Get(args.tree) != None:
-                tfile.ttree = tfile.Get(args.tree)
-                ttrees += [tfile.ttree]
-        if not hasattr(tfile,'ttree'):
-            print("didn't find any ttree inside %s matching the listOfKnownTrees, printing out available keys"%tfile.GetName())
-            printListOfkeys(tfile)
-            pickUpTTree = raw_input('would you like to pickup one ttree from the list of %s ? (y/[n]):'%tfiles[0].GetName()).lower()
-            if(pickUpTTree in yes):
-                args.tree = raw_input('ttree name:')
-                tfile.ttree = tfile.Get(args.tree)
-                ttrees += [tfile.ttree]
-        if args.var != '':tfile.var = args.var
-        if hasattr(tfile,'ttree') and not hasattr(tfile,'var'):
-            pickUpVar = raw_input('no variables to process were given, would you like to pickup one ? (y/[n]):').lower() 
-            if(pickUpVar in yes): 
-                printListOfLeaves(tfile.ttree)
-                tfile.var = raw_input('variable to plot:')
-            if len(tfiles): useThisVarFromNowOn = raw_input('would you like to use the same variable for all remaining tfiles ? (y/[n]):').lower()
-            if useThisVarFromNowOn: args.var = tfile.var
-            if args.bins == '': binsFromInput = raw_input('no binning has been given for %s, would you like to give one ? (y/[n]):'%tfile.var).lower()
-            if binsFromInput in yes: 
-                args.bins = raw_input('please provide comma separated binning nBinsX, xMin, xMax, ... :') 
-                tfile.bins = args.bins
-            if args.xtitle =='': xtitleFromInput =  raw_input('no x-title would you like to give one ? (y/[n]):').lower() 
-            if xtitleFromInput in yes and args.bins != '': 
-                args.xtitle = raw_input('please provide x title:')
-            else: args.xtitle = args.var.split(':')[0]
-            if args.ytitle =='': ytitleFromInput =  raw_input('no y-title would you like to give one ? (y/[n]):').lower() 
-            if ytitleFromInput in yes and args.bins != '': 
-                args.ytitle = raw_input('please provide y-title:')
-                tfiles.ytitle = args.ytitle
-            else:  
-                if len(args.var.split(':'))==2: args.ytitle = args.var.split(':')[1]
-                else:
-                    args.ytitle='Events / bin'
-                    tfile.ytitle='Events / bin' 
-            # 1D histo
-            if len(args.var.split(':'))==1 and len(args.bins.split(','))==3:
-                print('creating a TH1F from given input')
-                nBins = int(tfile.bins.split(',')[0]) 
-                xMin  = float(tfile.bins.split(',')[1]) 
-                xMax  = float(tfile.bins.split(',')[2]) 
-                histo  = ROOT.TH1F(str(id(tfile.ttree))+str(args.var), ';%s;%s'%(args.xtitle, args.ytitle), nBins, xMin, xMax)            
-                maxEntries = tfile.ttree.GetEntries()
-                if args.goFast < 1.0: 
-                    maxEntries = int(args.goFast*maxEntries)
-                    print ("going fast with %d entries out of %d"%(maxEntries, ttrees[ii].GetEntries()))
-                tfile.ttree.Draw(str(args.var)+'>>'+histo.GetName(), str(args.sel), "goff", maxEntries)
-
-
-                 
-
-            
-                
-    
-#ttrees = [tfile.Get(args.tree) for tfile in tfiles]
-
-### global variable signaling that some plots are ready
-plotsReady = False
-
-
-if args.var == 'noVars' and args.files is not None and len(tfiles)>0 and hasattr(tfiles[0],'ttree'):
-    pickUpVar = raw_input('no variables to process were given, would you like to pickup one from the list of %s ? (y/[n])'%tfiles[0].GetName()).lower() 
-    if(pickUpVar in yes): 
-        printListOfLeaves(ttrees[0])
-        args.var = raw_input('variable to plot:')
-    for ii, tfile in enumerate(tfiles):
-        print("Opened tfiles[%d] = %s with ttree[%d] = %s"%(ii, tfile.GetName(), ii, ttrees[ii].GetName()))
-        
-if args.bins =='100,0,100' and args.var!= 'noVars':
-    getBins = raw_input('no binning has been given, use the default \'100,0,100\' ? [press enter for default or give binning to be used]')  
-    if(getBins!=''): args.bins = getBins
-
-
-### make histograms of the same 1 variable stored in identacally structured ttrees, stored in different files
-if isinstance(args.var, str) and args.var != '':
-    binning = args.bins.split(',')
-    nBins, xMin, xMax = int(binning[0]), float(binning[1]), float(binning[2])
-    if not args.axesTitles: args.axesTitles = '%s,Events'%(str(args.var))
-    histos = [ROOT.TH1F(str(id(ttree))+str(args.var), str(args.axesTitles), nBins, xMin, xMax) for ttree in ttrees]
-    colors = [1, 2, 4, 6, 7, 8, 9]
-    styles = [1, 2, 3, 4, 5, 6, 7]
-    for ii, histo in enumerate(histos): 
-        histo.Sumw2()
-        if args.leg != '':histo.SetTitle(args.leg.split(',')[ii])
-        if args.leg == '':histo.SetTitle(tfiles[ii].GetName())
-        histo.GetXaxis().SetTitle(args.axesTitles.split(',')[0])
-        histo.GetYaxis().SetTitle(args.axesTitles.split(',')[1])
-        histo.SetLineWidth(3)
-        histo.SetLineColor(colors[ii])
-        histo.SetLineStyle(styles[ii])
-        if styles[ii] == 1: histo.SetLineWidth(2)
-        histo.GetXaxis().SetNdivisions(505)
-    for ii, histo in enumerate(histos):
-        maxEntries = ttrees[ii].GetEntries()
-        if args.goFast < 1.0: 
-            maxEntries = int(args.goFast*maxEntries)
-            print ("going fast with %d entries out of %d"%(maxEntries, ttrees[ii].GetEntries()))
-        ttrees[ii].Draw(str(args.var)+'>>'+histos[ii].GetName(), str(args.sel), "goff", maxEntries)
-    plotsReady = True
-
- 
-
-
-
-
-
-if plotsReady:
-    can1.Update()
-
-### plot histograms
-if plotsReady:
-    can1 = ROOT.TCanvas()
-    can1.cd()
-    if args.logy: can1.SetLogy()
-    if args.logx: can1.SetLogx()
-    histos = sorted(histos, key = lambda h : -h.GetBinContent(h.GetMaximumBin()))
-    for ii,histo in enumerate(histos):
-        if ii==0: 
-            histos[ii].Draw("hist")
-        else:
-            histos[ii].Draw("hist same")    
-    
-    lx1, ly1, lx2, ly2 = 0.65, 0.8 , 0.93, 0.93
-    leg = ROOT.TLegend(lx1, ly1, lx2, ly2)
-    leg.SetTextSize(26)
-    leg.SetTextFont(43)
-    leg.SetFillColor(ROOT.kWhite)
-    leg.SetFillStyle(0)
-    leg.SetBorderSize(0)
-    for histo in histos:leg.AddEntry(histo, histo.GetTitle(), 'l')
-    leg.Draw("same")
-'''
-
-
-
-
-
-
-
-
-
-
 
 
 
